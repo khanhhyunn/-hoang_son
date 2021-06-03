6084fbf2e306b83302016e3846019457b3669acf
khanhhyunn <85271902+khanhhyunn@users.noreply.github.com>
2021-06-03 18:15:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnNo: 'Không 😢 ',
     question:' Đố anh biết trên thế giới hơn 7 tỉ người mà tại sao em lại yêu anh đấy ♥️',
     btnReply: 'Gửi cho công chúa ♥️',
-    reply: 'Suỵt😗Cái này là bí mật',
+    reply: 'Suỵt😗Cái này là bí mật, bao cưới em về thì em nói cho :P ',
     mess: 'Finally,I know your day has been STRESSFUL 😓',
     messDesc: ' BUT REMEMBER 😘 ',
     btnAccept: 'That I always be there for you ♥️ ',
