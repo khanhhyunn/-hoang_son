61180ef5cd68b46321e3d45decc71578003af751
khanhhyunn <85271902+khanhhyunn@users.noreply.github.com>
2021-06-03 17:13:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const CONFIG = {
     btnNo: 'Không 😢 ',
     question:' Đố anh biết trên thế giới hơn 7 tỉ người mà sao em lại yêu anh đấy ♥️',
     btnReply: 'Gửi cho công chúa ♥️',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'I know your day has been STRESSFUL 😓',
+    reply: 'Suỵt 😗 Cái này là bí mật, bao giờ cưới em về thì em nói cho 😛',
+    mess: 'Finally,I know your day has been STRESSFUL 😓',
     messDesc: ' BUT REMEMBER 😘 ',
     btnAccept: 'That I always be there for you ♥️ ',
     messLink: 'https://www.facebook.com/lim.choding.9/' //link mess của các bạn. VD: https://m.me/nam.nodemy
