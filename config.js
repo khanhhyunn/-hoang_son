79980f869ae3da2ec2e32752931c7db4576b674f
khanhhyunn <85271902+khanhhyunn@users.noreply.github.com>
2021-06-03 15:25:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho công chúa <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'I know your day has been STRESSFUL ',
+    messDesc: ' BUT REMEMBER ',
+    btnAccept: 'That I always be there for you ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
