09bfa51a1bfb74818d076c7f0828e575297917b6
khanhhyunn <85271902+khanhhyunn@users.noreply.github.com>
2021-06-03 18:03:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     title: 'Anh có yêuu em hông? 😙',
     btnYes: 'Có 🥰 ',
     btnNo: 'Không 😢 ',
-    question:' Đố anh biết trên thế giới hơn 7 tỉ người mà sao em lại yêu anh đấy ♥️',
+    question:' Đố anh biết trên thế giới hơn 7 tỉ người mà tại sao em lại yêu anh đấy ♥️',
     btnReply: 'Gửi cho công chúa ♥️',
     reply: 'Suỵt😗Cái này là bí mật, bao giờ cưới em về thì em nói cho 😛',
     mess: 'Finally,I know your day has been STRESSFUL 😓',
